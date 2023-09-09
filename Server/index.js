268b04cadb522c35ef9e4f1396369f5494a93657
Gaurav Verma <137498654+Gaurav23V@users.noreply.github.com>
2023-09-09 11:38:32 +0000

    b

--- Server/index.js
+++ Server/index.js
@@ -6,12 +6,17 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 const corsOptions = {
   origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
+  credentials: true,
   optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+// Define a route for the root URL
+app.get("/", (req, res) => {
+  res.send("Hello, this is the root route!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
