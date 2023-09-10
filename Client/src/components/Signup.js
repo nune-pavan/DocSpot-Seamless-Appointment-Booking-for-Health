54462d7a354e4c35a29764df922a061b9aa06899
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2023-09-10 06:59:32 +0530

    user authentication completed along with login and signup functionality.

--- Client/src/components/Signup.js
+++ Client/src/components/Signup.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
 
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
@@ -10,15 +10,97 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
-import Divider from "@mui/material/Divider";
 import Link from "@mui/material/Link";
 import Footer from "./Footer";
 
 const Signup = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "name") {
+      setName(value);
+    } else if (id === "email") {
+      setEmail(value);
+    } else if (id === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Step 1: Sign up the user and get the user _id
+      const userResponse = await fetch(
+        "http://localhost:9000/api/users/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password, role }),
+        }
+      );
+
+      if (userResponse.ok) {
+        const { _id } = await userResponse.json();
+
+        // Step 2: Store additional data in the appropriate collection based on the role
+        if (role === "doctor") {
+          const doctorResponse = await fetch(
+            "http://localhost:9000/api/doctors",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ _id, name, email }),
+            }
+          );
+
+          if (doctorResponse.ok) {
+            // Handle successful doctor registration
+            console.log("Doctor registered successfully");
+            navigate("/login");
+          } else {
+            // Handle error response
+            console.error("Failed to register doctor");
+          }
+        } else if (role === "patient") {
+          const patientResponse = await fetch(
+            "http://localhost:9000/api/patients",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ _id, name, email }),
+            }
+          );
+
+          if (patientResponse.ok) {
+            // Handle successful patient registration
+            console.log("Patient registered successfully");
+            navigate("/login");
+          } else {
+            // Handle error response
+            console.error("Failed to register patient");
+          }
+        }
+      } else {
+        // Handle error response
+        console.error("Failed to sign up user");
+      }
+    } catch (error) {
+      // Handle network or server error
+      console.error("Server error:", error);
+    }
+  };
 
   return (
     <div className="w-full flex flex-col items-center bg-webgrey">
@@ -35,7 +117,7 @@ const Signup = () => {
           </p>
         </div>
         <div className="flex flex-col self-center justify-center items-center bg-white shadow-lg w-1/3 py-8 mt-6 mb-8 rounded-lg text-webgrey">
-          <form className="flex flex-col w-5/6 gap-6">
+          <form className="flex flex-col w-5/6 gap-6" onSubmit={handleSubmit}>
             <TextField
               id="name"
               type="text"
@@ -43,7 +125,7 @@ const Signup = () => {
               variant="outlined"
               required
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={handleInputChange}
             />
             <TextField
               id="email"
@@ -52,7 +134,7 @@ const Signup = () => {
               variant="outlined"
               required
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleInputChange}
             />
             <TextField
               type="password"
@@ -62,7 +144,7 @@ const Signup = () => {
               variant="outlined"
               required
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleInputChange}
             />
 
             <FormControl>
@@ -77,6 +159,9 @@ const Signup = () => {
                     row
                     aria-labelledby="demo-row-radio-buttons-group-label"
                     name="row-radio-buttons-group"
+                    onChange={(e) => {
+                      setRole(e.target.value);
+                    }}
                   >
                     <FormControlLabel
                       value="doctor"
@@ -93,7 +178,9 @@ const Signup = () => {
               </div>
             </FormControl>
 
-            <SignupButton variant="contained">Sign Up</SignupButton>
+            <SignupButton variant="contained" type="submit">
+              Sign Up
+            </SignupButton>
             <div className="flex flex-row gap-1 text-center justify-center">
               <div> Already have an account? </div>
               <div className="text-black">
