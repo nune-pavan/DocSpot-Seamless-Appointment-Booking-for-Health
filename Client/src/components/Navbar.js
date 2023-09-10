18b8cd7f3b29d536645dac565fcb7e5ecf7cec96
Yogit <you@example.com>
2023-09-10 19:22:05 +0530

    logout

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 function Navbar() {
+  const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
   const handleShowMenu = () => {
     setShowMenu((preve) => !preve);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    toast("Logged out Successfully!");
+    navigate("/");
   };
   return (
     <header className="fixed w-full h-16 bg-white z-50">
@@ -17,11 +24,29 @@ function Navbar() {
               Doc<span className="text-webnavyblue">Spot</span>
             </h1>
           </Link>
-          
+
           <div onClick={handleShowMenu}>
-          <AccountBoxIcon style={{ fontSize: "4rem", color: "#2B2D42" }} />
+            <AccountBoxIcon style={{ fontSize: "3rem", color: "#2B2D42" }} />
+            {showMenu && (
+              <div className="absolute bg-white py-2 px-2 shadow drop-shadow-md flex flex-col">
+                {localStorage.getItem("id") ? (
+                  <p
+                    className="cursor-pointer lg:text-xs xl:text-sm"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </p>
+                ) : (
+                  <Link
+                    to={"login"}
+                    className="whitespace-nowrap cursor-pointer"
+                  >
+                    SignIn
+                  </Link>
+                )}
+              </div>
+            )}
           </div>
-          
         </div>
       </div>
     </header>
