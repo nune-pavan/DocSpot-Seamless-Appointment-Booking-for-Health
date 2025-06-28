51f81ae22981c7c711d84f2c1d33030cc39d643b
nune-pavan <228A1A05C1@risekrishnasaiprakasam.edu.in>
2025-06-28 10:27:07 +0530

    Update db.js

--- project files/Server/utils/db.js
+++ project files/Server/utils/db.js
@@ -2,18 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async (dbURI) => {
   try {
-    if (!dbURI) {
-      throw new Error("❌ DB_URI is undefined. Make sure it's set correctly.");
-    }
-
     await mongoose.connect(dbURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log("✅ Connected to MongoDB");
   } catch (error) {
-    console.error("❌ Failed to connect to MongoDB:", error.message);
+    console.error("❌ Failed to connect to MongoDB", error);
     process.exit(1);
   }
 };
